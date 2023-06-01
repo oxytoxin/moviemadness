c3374b52c565d8b2c30431ca7a1ecc567194b16b
oxytoxin <oxytoxinsgrace@gmail.com>
2023-06-01 12:36:19 +0800

    removed animejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,10 @@
 import './bootstrap';
 import 'remixicon/fonts/remixicon.css'
 import Alpine from 'alpinejs'
-import anime from 'animejs';
 import { Splide } from '@splidejs/splide';
 import '@splidejs/splide/css';
 
 
-window.anime = anime;
 window.Splide = Splide;
 window.Alpine = Alpine
 
