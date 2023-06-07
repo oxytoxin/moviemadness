0533d6648691448bdf2543f49191fbfe0dda3f6a
oxytoxin <oxytoxinsgrace@gmail.com>
2023-06-07 15:35:49 +0800

    test webhook

--- app/Http/Controllers/GithubWebhookController.php
+++ app/Http/Controllers/GithubWebhookController.php
@@ -11,17 +11,5 @@ class GithubWebhookController extends Controller
     public function pull()
     {
         exec('php artisan down');
-        try {
-            exec('git pull');
-            exec('pnpm install');
-            exec('pnpm build');
-            exec('composer install --no-dev --optimize-autoloader --no-interaction');
-            exec('php artisan up');
-            return response('Pull from repository successful.', 200);
-        } catch (\Throwable $th) {
-            info('Unable to pull from repository.');
-            abort(500);
-        }
-        exec('php artisan up');
     }
 }
