68b873544191ae70f0e81c6b30d520a6a60d9b40
oxytoxin <oxytoxinsgrace@gmail.com>
2023-06-07 15:42:05 +0800

    test webhook

--- app/Http/Controllers/GithubWebhookController.php
+++ app/Http/Controllers/GithubWebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Process;
 
 use function PHPSTORM_META\map;
 
@@ -10,7 +11,6 @@ class GithubWebhookController extends Controller
 {
     public function pull()
     {
-        // testsss
-        exec('php artisan down');
+        Process::run('php artisan down');
     }
 }
